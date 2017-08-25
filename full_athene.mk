1fce423b525eeeec74507c47f4f4661ece5b92c8
Kukun Mishra <kukun7610@gmail.com>
2017-08-25 16:04:06 +0530

    Update full_athene.mk

--- full_athene.mk
+++ full_athene.mk
@@ -21,6 +21,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from athene device
 $(call inherit-product, device/motorola/athene/device.mk)
 
+
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := athene
 PRODUCT_NAME := full_athene
