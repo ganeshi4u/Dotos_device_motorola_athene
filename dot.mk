0ddbfbcebd5d70347322d58cb219f8ca0d47d903
Jashkirat <jashkiratsingh@gmail.com>
2017-08-26 11:00:04 +0530

    Update and rename dosp_athene.mk to dot.mk

--- dot.mk
+++ dot.mk
@@ -14,7 +14,7 @@
 # limitations under the License.
 
 # Inherit some common aosp stuff.
-$(call inherit-product, vendor/dosp/common.mk)
+$(call inherit-product, vendor/dot/common.mk)
 
 $(call inherit-product, device/motorola/athene/full_athene.mk)
 
@@ -25,7 +25,7 @@ DEFAULT_ROOT_METHOD := magisk
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := athene
-PRODUCT_NAME := dosp_athene
+PRODUCT_NAME := dot_athene
 PRODUCT_BRAND := Motorola
 PRODUCT_MANUFACTURER := Motorola
 PRODUCT_RELEASE_NAME := athene
